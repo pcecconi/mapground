48aa38b983246d2f36f98d2716ca418ed6a5d43c
Cristian <czunino@gmail.com>
2018-06-24 22:32:12 -0300

    Fix: render_to_response --> render (metodos deprecados)

--- layers/views.py
+++ layers/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 # core
 from django.http import HttpResponse,HttpResponseRedirect, HttpResponseForbidden
-from django.template import RequestContext, loader
+from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.conf import settings
