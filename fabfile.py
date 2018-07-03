fe6bded71481645b7e785a32e77fe6b3de7d201c
Pablo Cecconi <pcecconi@gmail.com>
2018-07-03 19:30:22 -0300

    Scripts BASH para entorno de dev

--- fabfile.py
+++ fabfile.py
@@ -10,48 +10,7 @@ def _get_secret_key():
 	return "".join([random.SystemRandom().choice(string.digits + string.letters) for i in range(100)])
 
 def setup_dev():
-	# _install_local_base_packages()
-	dbname = 'mapground_dev'
-	dbpass = prompt('Ingrese el password para asignar al usuario de la base de datos:')
-	# _setup_local_db(dbname, dbname, dbpass)
-	local("sudo ./setup_db.sh -d=%s -u=%s -p=%s"%(dbname,dbname,dbpass))
-	secret_key = _get_secret_key()
-	dir = os.getcwd()
-	local("sed -e 's/mapground-db/%s/g' MapGround/settings_db.py.template | sed -e 's/mapground-user/%s/g' - | sed -e 's/mapground-password/%s/g' - | sed -e 's/secret-key/%s/g' > MapGround/settings_dev_db.py" % (dbname, dbname, dbpass, secret_key))
-	local("cp MapGround/settings_local.py.template MapGround/settings_local.py")
-	# local('mkdir MapGround/media; mkdir mapcache/cache; touch mapfiles/map-error.log; sudo chmod 666 mapfiles/map-error.log')
-	try:
-		local('sudo rm -rf /var/cache/mapground_dev')
-		local('sudo rm -rf /var/local/mapground_dev')
-	except:
-		pass
-	local('sudo mkdir /var/cache/mapground_dev; sudo mkdir /var/local/mapground_dev; sudo mkdir /var/local/mapground_dev/media')
-	local('sudo cp -r mapfiles /var/local/mapground_dev; sudo cp -r data /var/local/mapground_dev; sudo chown -R ${USER:=$(/usr/bin/id -run)}:$USER /var/local/mapground_dev; touch /var/local/mapground_dev/mapfiles/map-error.log; sudo chmod 666 /var/local/mapground_dev/mapfiles/map-error.log')
-	local("sed -e 's/\DEBUG = False$/DEBUG = True/' mapcache/settings.py.template > mapcache/settings.py")
-	local('cp mapcache/mapcache.xml.template /var/local/mapground_dev/mapcache.xml')
-	# local('cp mapcache/settings.py.template mapcache/settings.py')
-	# local('sudo chown -R www-data:www-data mapcache/cache')
-	local("sed -e 's/\/mapground\//\/mapground_dev\//g' mapground_apache.conf.template > mapground_dev_apache.conf")
-	local("sudo sed -i 's/^<VirtualHost \*:8080>$/<VirtualHost *:7654>/' mapground_dev_apache.conf")
-	try:
-		local('sudo rm /etc/apache2/sites-enabled/mapground_dev.conf')
-	except:
-		pass
-	local("sudo cp mapground_dev_apache.conf /etc/apache2/sites-available/mapground_dev.conf")
-	local("sudo ln -s /etc/apache2/sites-available/mapground_dev.conf /etc/apache2/sites-enabled/mapground_dev.conf")
- 	local("deactivate; virtualenv --system-site-packages venv; source venv/bin/activate; pip install -r requirements.txt", shell='/bin/bash')
- 	# local("cp venv_path_extensions.pth venv/lib/python2.7/site-packages/")
-	# local("source venv/bin/activate; python manage.py syncdb --noinput", shell='/bin/bash')
-	local("source venv/bin/activate; python manage.py makemigrations", shell='/bin/bash')
-	local("source venv/bin/activate; python manage.py migrate", shell='/bin/bash')
-	local("source venv/bin/activate; python manage.py loaddata layers/fixtures/initial_data.json", shell='/bin/bash')
-	local("source venv/bin/activate; python manage.py loaddata MapGround/fixtures/user.json", shell='/bin/bash')
-	local('source venv/bin/activate; mapcache/manage.py add world_borders', shell='/bin/bash')
-	local('sudo chown -R www-data:www-data /var/cache/mapground_dev; sudo chmod g+rwxs /var/cache/mapground_dev')
-	local('sudo chmod 666 /var/local/mapground_dev/mapcache.xml;')
-	local('sudo service apache2 restart')
-	# local('sudo -u postgres psql -c "alter role %s createdb; alter role %s superuser;"' % (dbname, dbname))
-	# local("source venv/bin/activate; python manage.py test", shell='/bin/bash')
+	local("sudo ./setup_dev.sh")
 
 def setup_prod():
 	local("sudo ./setup.sh -h=localhost")
---
+++ setup_dev.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+
+# Nos aseguramos de que el script se corra como root
+if [ "$(id -u)" != "0" ]; then
+   echo "Este script debe correrse root" 1>&2
+   exit 1
+fi
+
+export LC_ALL='es_AR.UTF-8'
+export LANGUAGE='es_AR.UTF-8'
+export LANG='es_AR.UTF-8'
+
+for i in "$@"
+do
+case $i in
+    -d=*|--dbname=*)
+    dbname="${i#*=}"
+    ;;
+    -u=*|--dbuser=*)
+    dbuser="${i#*=}"
+    ;;
+    -p=*|--dbpass=*)
+    dbpass="${i#*=}"
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
+
+if [ -z "$dbname" ]; then dbname='mapground_dev'; echo "Base de datos por defecto: $dbname"; fi
+if [ -z "$dbuser" ]; then dbuser='mapground_dev'; echo "Usuario de base de datos por defecto: $dbuser"; fi
+
+echo "Configurando base de datos en localhost...";
+
+if [ -z "$dbpass" ]; then
+  read -s -p "Ingrese el password para asignar al usuario de la base de datos:" dbpass;
+  echo;
+fi
+
+./setup_db.sh -d=$dbname -u=$dbuser -p=$dbpass ;
+
+SECRET_KEY=$(python mk_secret_key.py)
+
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+
+DEV_USER="$(ls -ld $0 | awk '{print $3}')"
+
+sed -e "s/mapground-db/${dbname}/g" MapGround/settings_db.py.template | sed -e "s/mapground-user/${dbname}/g" - | sed -e "s/mapground-password/${dbpass}/g" - | sed -e "s/secret-key/${SECRET_KEY}/g" > MapGround/settings_dev_db.py
+cp MapGround/settings_local.py.template MapGround/settings_local.py
+chown $DEV_USER MapGround/settings_dev_db.py MapGround/settings_local.py
+
+rm -rf /var/cache/mapground_dev 2>/dev/null
+rm -rf /var/local/mapground_dev 2>/dev/null
+
+mkdir /var/cache/mapground_dev
+mkdir /var/local/mapground_dev
+mkdir /var/local/mapground_dev/media
+cp -r mapfiles /var/local/mapground_dev
+touch /var/local/mapground_dev/mapfiles/map-error.log
+chmod 666 /var/local/mapground_dev/mapfiles/map-error.log
+cp -r data /var/local/mapground_dev
+chown -R $DEV_USER /var/local/mapground_dev
+cp mapcache/mapcache.xml.template /var/local/mapground_dev/mapcache.xml
+sed -e 's/\DEBUG = False$/DEBUG = True/' mapcache/settings.py.template > mapcache/settings.py
+
+sed -e "s:/mapground/:/mapground_dev/:g" mapground_apache.conf.template > /etc/apache2/sites-available/mapground_dev.conf
+sed -i "s/^<VirtualHost \*:8080>$/<VirtualHost *:7654>/" /etc/apache2/sites-available/mapground_dev.conf
+rm /etc/apache2/sites-enabled/mapground_dev.conf 2>/dev/null
+ln -s /etc/apache2/sites-available/mapground_dev.conf /etc/apache2/sites-enabled/mapground_dev.conf
+
+sudo -u ${DEV_USER} bash -c 'virtualenv --system-site-packages venv; source venv/bin/activate; pip install -r requirements.txt'
+
+source venv/bin/activate
+
+./mapcache/manage.py add world_borders
+# python manage.py makemigrations
+python manage.py migrate
+python manage.py loaddata MapGround/fixtures/user.json
+python manage.py loaddata layers/fixtures/initial_data.json
+
+deactivate
+
+chown -R www-data:www-data /var/cache/mapground_dev
+chmod g+rwxs /var/cache/mapground_dev
+chmod 666 /var/local/mapground_dev/mapcache.xml
+
+service apache2 restart
