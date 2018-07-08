3ed432d5913020ad696f4056e6362df75b967baf
Cristian <czunino@gmail.com>
2018-07-07 22:50:01 -0300

    Metodos para pasar int a base62 y al reves
    PEP8

--- utils/commons.py
+++ utils/commons.py
@@ -15,6 +15,7 @@ from django.contrib.gis.geos import Point
 
 nombre_de_funcion_de_javascript_valido = re.compile(r"^[$A-Z_][0-9A-Z_$]*$", re.IGNORECASE)
 
+
 def es_un_nombre_de_funcion_de_javascript_valido(nombre_de_funcion_de_javascript):
     if nombre_de_funcion_de_javascript == '':
         return False
@@ -22,25 +23,27 @@ def es_un_nombre_de_funcion_de_javascript_valido(nombre_de_funcion_de_javascript
         return False
     if ((re.match(nombre_de_funcion_de_javascript_valido, nombre_de_funcion_de_javascript)) is None):
         return False
-    return True    
+    return True
+
 
 # dados un string de dato y un string de callback, aplica el callback si tiene un nombre de función javascript válido
 def aplicar_callback(data, callback):
-    # TODO: ver si conviene agregar el chequeo de string de data 
+    # TODO: ver si conviene agregar el chequeo de string de data
     if es_un_nombre_de_funcion_de_javascript_valido(callback):
-        return callback+'('+data+')'
+        return callback + '(' + data + ')'
     return data
-    
+
+
 # normaliza un string quitándole acentos y caracteres especiales
 def normalizar_texto(texto, to_lower=True):
     if to_lower:
         texto = unicode(texto).lower()
-    texto = ''.join((c for c in unicodedata.normalize('NFD', texto) if unicodedata.category(c) != 'Mn')) # reemplazamos ñ y acentos por n y sin acentos
-    #texto = re.sub(unicode(r'[^\w\d\sáÁäÄéÉëËíÍïÏóÓöÖúÚüÜÿŸñÑ]','utf-8'),lambda mo: '',texto)
+    texto = ''.join((c for c in unicodedata.normalize('NFD', texto) if unicodedata.category(c) != 'Mn'))  # reemplazamos ñ y acentos por n y sin acentos
+    # texto = re.sub(unicode(r'[^\w\d\sáÁäÄéÉëËíÍïÏóÓöÖúÚüÜÿŸñÑ]','utf-8'),lambda mo: '',texto)
 
-    texto = re.sub(unicode(r'[^\w\d\s]','utf-8'),lambda mo: '', re.sub('([,;:./-])(?!\s)', r'\1 ', re.sub('\.(?!(\S[^. ])|\d)', '', texto))) #separa siglas separadas por . , : . / -
-#    texto = re.sub(unicode(r'[^\w\d\s]','utf-8'),lambda mo: ' ',texto) # reemplazamos caracteres especiales por espacios
-    texto = re.sub(unicode(r'[\s_]+','utf-8'), lambda mo: '_', texto) # reemplazamos cadenas de espacios o de _ por un _
+    texto = re.sub(unicode(r'[^\w\d\s]', 'utf-8'), lambda mo: '', re.sub('([,;:./-])(?!\s)', r'\1 ', re.sub('\.(?!(\S[^. ])|\d)', '', texto)))  # separa siglas separadas por . , : . / -
+    # texto = re.sub(unicode(r'[^\w\d\s]','utf-8'),lambda mo: ' ',texto) # reemplazamos caracteres especiales por espacios
+    texto = re.sub(unicode(r'[\s_]+', 'utf-8'), lambda mo: '_', texto)  # reemplazamos cadenas de espacios o de _ por un _
     texto = texto.strip('_')
     return texto
 
@@ -60,15 +63,16 @@ def paginar_y_elegir_pagina(lista_objetos, pagina, cantidad_por_pagina):
 
 # convierte un QueryDict a Dict
 def queryDictToDict(request, omitir_callback=True):
-    request_dict=dict()
+    request_dict = dict()
     for item in request.GET.items():
-        if not (omitir_callback and (item[0]=='callback' or item[0]=='_')):
-            request_dict[item[0]]=item[1]
+        if not (omitir_callback and (item[0] == 'callback' or item[0] == '_')):
+            request_dict[item[0]] = item[1]
     return request_dict
+
 
 # parsea un string de tipo 'Comma-Separated-Integers' y devuelve una lista
 def parsear_csi_input(texto):
-    res=[]
+    res = []
     if texto != '':
         texto = texto.split(',')
         for s in texto:
@@ -78,9 +82,10 @@ def parsear_csi_input(texto):
                 pass
     return res
 
+
 # parsea un string de tipo 'Comma-Separated-Strings' y devuelve una lista
 def parsear_css_input(texto):
-    res=[]
+    res = []
     if texto != '':
         texto = texto.split(',')
         for s in texto:
@@ -90,17 +95,19 @@ def parsear_css_input(texto):
                 pass
     return res
 
+
 # parsea un string que representa un bbox separado por comas y devuelve None o una 4-upla
 def parsear_bbox(str_bbox):
     try:
-        vals=str_bbox.split(',')
+        vals = str_bbox.split(',')
         if len(vals) != 4:
             return None
         for v in vals:
-            v= float(v)
+            v = float(v)
         return vals
     except:
         return None
+
 
 def urlToFile(url, filename):
     try:
@@ -108,11 +115,11 @@ def urlToFile(url, filename):
         opener = urllib2.build_opener(proxy)
         urllib2.install_opener(opener)
 
-        with open(filename,'wb') as f:
+        with open(filename, 'wb') as f:
             f.write(urllib2.urlopen(url).read())
             f.close()
     except:
-        print "Error saving url %s to file %s"%(url, filename)
+        print "Error saving url %s to file %s" % (url, filename)
         return False
     return True
 
@@ -121,7 +128,53 @@ def coordConvert(lat, lng, sridFrom, sridTo):
     fromCoord = SpatialReference(sridFrom)
     toCoord = SpatialReference(sridTo)
     trans = CoordTransform(fromCoord, toCoord)
-    
+
     pnt = Point(lat, lng, srid=sridFrom)
     pnt.transform(trans)
     return pnt
+
+
+BASE62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+def encode_to_base(num, alphabet=BASE62):
+    """Encode a positive number in Base X.
+
+    Arguments:
+    - `num`: The number to encode
+    - `alphabet`: The alphabet to use for encoding
+    """
+    try:
+        int(num)
+    except:
+        return None
+    num = abs(int(num))
+    if num == 0:
+        return alphabet[0]
+    arr = []
+    base = len(alphabet)
+    while num:
+        num, rem = divmod(num, base)
+        arr.append(alphabet[rem])
+    arr.reverse()
+    return ''.join(arr)
+
+
+def decode_from_base(string, alphabet=BASE62):
+    """Decode a Base X encoded string into the number.
+
+    Arguments:
+    - `string`: The encoded string
+    - `alphabet`: The alphabet to use for encoding
+    """
+    base = len(alphabet)
+    strlen = len(string)
+    num = 0
+
+    idx = 0
+    for char in string:
+        power = (strlen - (idx + 1))
+        num += alphabet.index(char) * (base ** power)
+        idx += 1
+
+    return num
