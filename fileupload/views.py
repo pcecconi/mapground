3cbfb84eb497b3ed662b69ea7e3f65db667b3dd9
Cristian <czunino@gmail.com>
2019-01-20 13:49:14 -0300

    Nuevo feature y modelo: permiso de upload de capas para los usuarios: modelo, admin, migraciones y logica en template

--- fileupload/views.py
+++ fileupload/views.py
@@ -2,14 +2,21 @@
 from __future__ import unicode_literals
 
 from django.views.generic import CreateView, DeleteView, ListView
+from django.contrib.auth.mixins import UserPassesTestMixin
 from .models import Archivo
 from .response import JSONResponse, response_mimetype
 from .serialize import serialize
 
 
-class ArchivoCreateView(CreateView):
+class ArchivoCreateView(UserPassesTestMixin, CreateView):
     model = Archivo
     fields = ['file']
+
+    # Cambio el signature de esta vista: heredo de UserPassesTestMixin y agrego el siguiente metodo para evitar acceso si no tiene permiso de upload
+    # https://stackoverflow.com/questions/4597401/django-user-permissions-to-certain-views?noredirect=1&lq=1
+    # respuesta de 'nesdis'
+    def test_func(self):
+        return self.request.user.userprofile.puede_subir_capas
 
     def form_valid(self, form):
         form.instance.owner = self.request.user
@@ -20,6 +27,7 @@ class ArchivoCreateView(CreateView):
         response = JSONResponse(data, mimetype=response_mimetype(self.request))
         response['Content-Disposition'] = 'inline; filename=files.json'
         return response
+
 
 class ArchivoActualizacionCreateView(CreateView):
     template_name = 'archivo_update_form.html'
--- users/admin.py
+++ users/admin.py
@@ -2,25 +2,37 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from users.models import PermisoDeCapa, PermisoDeMapa, PermisoDeCapaPorGrupo
+from users.models import UserProfile, PermisoDeCapa, PermisoDeMapa, PermisoDeCapaPorGrupo
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+    list_display = ['id', 'user', 'puede_subir_capas']
+    list_display_links = ['id', ]
+    ordering = ["-id"]
+    list_filter = ['puede_subir_capas']
+
+admin.site.register(UserProfile, UserProfileAdmin)
+
 
 class PermisoDeCapaAdmin(admin.ModelAdmin):
-    readonly_fields=('id',)
+    readonly_fields = ('id',)
     search_fields = ('user__username', 'capa__id_capa', 'permiso')
-    list_display = ['id', 'user', 'capa', 'permiso',]
-    list_display_links = ['id',]
+    list_display = ['id', 'user', 'capa', 'permiso']
+    list_display_links = ['id', ]
     ordering = ["-id"]
-    list_filter = ['permiso', 'user', 'capa',]
+    list_filter = ['permiso', 'user', 'capa']
 
 admin.site.register(PermisoDeCapa, PermisoDeCapaAdmin)
 
+
 class PermisoDeCapaPorGrupoAdmin(admin.ModelAdmin):
-    readonly_fields=('id',)
+    readonly_fields = ('id',)
     search_fields = ('group__name', 'capa__id_capa', 'permiso')
-    list_display = ['id', 'group', 'capa', 'permiso',]
-    list_display_links = ['id',]
+    list_display = ['id', 'group', 'capa', 'permiso']
+    list_display_links = ['id', ]
     ordering = ["-id"]
-    list_filter = ['permiso', 'group', 'capa',]
+    list_filter = ['permiso', 'group', 'capa']
 
 admin.site.register(PermisoDeCapaPorGrupo, PermisoDeCapaPorGrupoAdmin)
 
